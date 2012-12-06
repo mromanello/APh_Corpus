341f61a2de947649e21bcdac9fa123c44028c347
Matteo Romanello <matteo.romanello@gmail.com>
2012-12-06 22:59:59 +0100

    committing last uncommitted changes

--- example.py
+++ example.py
@@ -18,6 +18,15 @@ for id in newdict.keys():
         bylang[newdict[id]['lang']]+=[id]
 print bylang
 
+bycoll = {}
+for id in newdict.keys():
+    if(bycoll.has_key(newdict[id]['collection'])):
+        bycoll[newdict[id]['collection']]+=[id]
+    else:
+        bycoll[newdict[id]['collection']]=[]
+        bycoll[newdict[id]['collection']]+=[id]
+print bycoll
+
 # prints language and number of abstracts for each language
 total = 0
 for lang in bylang.keys():
@@ -37,3 +46,20 @@ for id in newdict.keys():
         file = open("iob/%s.txt"%id,"r")
     except Exception, e:
         print id,newdict[id]['collection']
+
+"""
+# the following lines copy 
+import os
+import shutil
+
+dir = "by_collection"
+
+for coll in bycoll:
+    os.mkdir("%s/%s"%(dir,coll))
+for coll in bycoll:
+    for file in bycoll[coll]:
+        try:
+            shutil.copy("iob/%s.txt"%file,"%s%s/%s.iob"%("by_collection/",coll,file))
+        except Exception, e:
+            print file,e
+"""
